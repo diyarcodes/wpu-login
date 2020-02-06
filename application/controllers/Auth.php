ade83e06d1a9c7e4006cbe12c9f4be7b1b865f00
Diyar.codes <diyar.codes16@gmail.com>
2020-02-07 02:16:23 +0700

    registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -16,8 +16,47 @@ class Auth extends CI_Controller
     {
         $data['judul'] = "Halaman Registration";
 
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/registrasi');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim', [
+            'required' => 'Nama Tidak Boleh Kosong'
+        ]);
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'required' => 'Email Tidak Boleh Kosong',
+            'valid_email' => 'Email Harus Benar',
+            'is_unique' => 'Email Sudah Terdaftar Sebelumnya'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'required' => 'Password Tidak Boleh Kosong',
+            'min_length' => 'Password Minimal Berisi 3 Karakter',
+            'matches' => 'Password Tidak Sama'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]', [
+            'required' => 'Password Tidak Boleh Kosong',
+            'matches' => 'Password Tidak Sama'
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registrasi');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+            User Berhasil Terdaftar
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>');
+            redirect('auth');
+        }
     }
 }
